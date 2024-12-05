c96bea057ed52dc81b0bc9ed63c3d14dcb4aa2d9
CEIT-Robot_Computer <hostingngrok@gmail.com>
2024-12-05 23:08:29 +0700

    set output response as html

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
 	"io"
 	"log"
 	"net/http"
 	"os/exec"
+	"os"
 	"path/filepath"
 )
 
 const port string = ":8080"
+var output string
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -19,7 +22,7 @@ func main() {
 			http.Error(w, "Server Error", http.StatusInternalServerError)
 			return
 		}
-		t.Execute(w, nil)
+		t.Execute(w, map[string]string{"Output": output,})
 	})
 
 	http.HandleFunc("/gimmeEXE", func(w http.ResponseWriter, r *http.Request) {
@@ -40,23 +43,42 @@ func main() {
 			return
 		}
 
-		cmd := exec.Command("cmd.exe", "/C") // "/bin/sh" for Linux
-		stdin, _ := cmd.StdinPipe()
-
-		go func() {
-			defer stdin.Close()
-			io.Copy(stdin, file)
-		}()
-
-		output, err := cmd.CombinedOutput()
+		fileContent, err := io.ReadAll(file)
 		if err != nil {
-			http.Error(w, fmt.Sprintf("%s", err), http.StatusInternalServerError)
+			http.Error(w, "Error reading file", http.StatusInternalServerError)
 			return
 		}
 
-		fmt.Println(string(output))
-		w.Header().Set("Content-Type", "text/plain")
-		w.Write(output)
+		tempFile, err := os.CreateTemp("./", "temp_*.exe") 
+		if err != nil {
+			http.Error(w, "Error creating temporary file", http.StatusInternalServerError)
+			return
+		}
+		defer os.Remove(tempFile.Name())
+
+		_, err = tempFile.Write(fileContent)
+		if err != nil {
+			http.Error(w, "Error writing to temporary file", http.StatusInternalServerError)
+			return
+		}
+
+		tempFile.Close()
+
+		var outBuffer, errBuffer bytes.Buffer
+		cmd := exec.Command("wine", tempFile.Name())
+		cmd.Stdout = &outBuffer
+		cmd.Stderr = &errBuffer
+
+		err = cmd.Run()
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error executing .exe: %v\n%s", err, errBuffer.String()), http.StatusInternalServerError)
+			return
+		}
+
+		output = fmt.Sprintf("%s", outBuffer.String())
+
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte(output))
 	})
 
 	fmt.Printf("Server started at http://localhost%s\n", port)
